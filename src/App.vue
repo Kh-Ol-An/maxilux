78c6f470773af9361425a9380e40d99e9ee7efb9
Oleg Khristenko <olegkhristenko@gmail.com>
2021-08-24 21:20:30 +0300

    downloaded logo and main background image; created main block

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Main />
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Main from './components/Main.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+export default {
+  name: 'App',
+
+  components: {
+    Main,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Элитные входные двери</h1>
+<!--    https://www.bork.ru/eShop/Air-Cleaners/a803/?-->
+<!--    https://www.laborsync.com/-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+}
+</script>
